f06987f26dd1039aae3705495b37886c210d2a34
BrayanStivel <167356321+BrayanStivel@users.noreply.github.com>
2024-05-06 21:53:49 -0500

    Eje50.kt

--- src/Eje50.kt
+++ src/Eje50.kt
@@ -1,14 +1,14 @@
 class Eje50 {
-    fun sumaNumerosPares() {
-        var suma = 0
-        for (i in 98 until 1003 step 2) {
+fun main() {
+    var suma = 0
+
+    for (i in 97..1003) {
+        if (i % 2 == 0) { // Verifica si el número es par
             suma += i
         }
-        println("La suma de todos los números pares entre 97 y 1003 es: $suma")
     }
 
-    fun main() {
-        sumaNumerosPares()
-    }
+    println("La suma de todos los números pares entre 97 y 1003 es: $suma")
+}
 
 }
