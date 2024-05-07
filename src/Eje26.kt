13547d56d92377748e530c14fee2dd1f5d83ae8f
BrayanStivel <167356321+BrayanStivel@users.noreply.github.com>
2024-05-06 21:36:22 -0500

    Eje26.kt

--- src/Eje26.kt
+++ src/Eje26.kt
@@ -1,19 +1,19 @@
 class Eje26 {
-    fun calcularExpresion(A: Int, B: Int, C: Int, D: Int): Double {
-        return if (D == 0) {
-            Math.pow((A - C).toDouble(), 2.0)
-        } else {
-            Math.pow((A - B).toDouble(), 3.0) / D
-        }
+fun main() {
+    val A = 5
+    val B = 3
+    val C = 2
+    val D = 1
+
+    val resultado = if (D == 0) {
+        (A - C) * (A - C)
+    } else {
+        (A - B) * (A - B) * (A - B) / D.toDouble()
     }
 
-    fun main() {
-        val A = 5
-        val B = 3
-        val C = 8
-        val D = 2
-        println("Resultado de la expresión: ${calcularExpresion(A, B, C, D)}")
-    }
+    println("El resultado es: $resultado")
+}
+
 
 
 }
