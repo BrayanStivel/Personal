9fbdb3e023fd275ddebddb5a0d2dbb2937d1b6da
BrayanStivel <167356321+BrayanStivel@users.noreply.github.com>
2024-05-06 21:16:08 -0500

    Eje2.kt

--- src/Eje2.kt
+++ src/Eje2.kt
@@ -1,5 +1,11 @@
 class Eje2 {
+  fun main() {
+    val capitalInicial = 1000.0 
+    val tasaInteresMensual = 0.02 
 
+    val capitalFinal = capitalInicial * (1 + tasaInteresMensual)
 
+    println("Después de un mes, el capital final será: $capitalFinal")
+}
 
 }
