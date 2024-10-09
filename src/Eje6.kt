e3d770204c8ded199a316bd9c4202b8eff0de6b3
BrayanStivel <167356321+BrayanStivel@users.noreply.github.com>
2024-10-09 13:11:45 -0500

    Delete src directory

--- src/Eje6.kt
+++
@@ -1,9 +0,0 @@
-class Eje6 {
-    val totalEstudiantes = 30
-    val hombres = 15
-    val mujeres = totalEstudiantes - hombres
-    val porcentajeHombres = (hombres.toDouble() / totalEstudiantes) * 100
-    val porcentajeMujeres = (mujeres.toDouble() / totalEstudiantes) * 100
-    println("Porcentaje de hombres: $porcentajeHombres%, Porcentaje de mujeres: $porcentajeMujeres%")
-
-}
--- src/Eje8.kt
+++
@@ -1,9 +0,0 @@
-class Eje8 {
-    val ladoA = 3.0
-    val ladoB = 4.0
-    val ladoC = 5.0
-    val p = (ladoA + ladoB + ladoC) / 2
-    val areaTriangulo = (p * (p - ladoA) * (p - ladoB) * (p - ladoC))
-    println("El área del triángulo es: $areaTriangulo")
-
-}
