17ff52e7d68746d5d08ea1ee49687ad053129edd
BrayanStivel <167356321+BrayanStivel@users.noreply.github.com>
2024-10-11 14:06:48 -0500

    Update 13-variables.cs

--- Net/13-variables.cs
+++ Net/13-variables.cs
@@ -1,6 +1,21 @@
-int tiempoSegundos, horas, minutos;
-Console.WriteLine("Ingrese el tiempo en segundos:");
-tiempoSegundos = int.Parse(Console.ReadLine());
-horas = tiempoSegundos / 3600;
-minutos = (tiempoSegundos % 3600) / 60;
-Console.WriteLine("Tiempo: " + horas + " horas y " + minutos + " minutos");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declaracion de variables
+        int tiempoSegundos, horas, minutos;
+
+        // Solicitar al usuario que ingrese el tiempo en segundos
+        Console.WriteLine("Ingrese el tiempo en segundos:");
+        tiempoSegundos = int.Parse(Console.ReadLine());
+
+        // Calcular horas y minutos
+        horas = tiempoSegundos / 3600;
+        minutos = (tiempoSegundos % 3600) / 60;
+
+        // Mostrar el resultado
+        Console.WriteLine("Tiempo: " + horas + " horas y " + minutos + " minutos");
+    }
+}
