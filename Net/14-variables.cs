9dafc49e5472db3021ffe219303c21821a31f522
BrayanStivel <167356321+BrayanStivel@users.noreply.github.com>
2024-10-11 14:07:05 -0500

    Update 14-variables.cs

--- Net/14-variables.cs
+++ Net/14-variables.cs
@@ -1,7 +1,22 @@
-double distanciaMetros, km, cm, mm;
-Console.WriteLine("Ingrese la distancia en metros:");
-distanciaMetros = double.Parse(Console.ReadLine());
-km = distanciaMetros / 1000;
-cm = distanciaMetros * 100;
-mm = distanciaMetros * 1000;
-Console.WriteLine("Distancia: " + km + " km, " + cm + " cm, " + mm + " mm");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declaracion de variables
+        double distanciaMetros, km, cm, mm;
+
+        // Solicitar al usuario que ingrese la distancia en metros
+        Console.WriteLine("Ingrese la distancia en metros:");
+        distanciaMetros = double.Parse(Console.ReadLine());
+
+        // Realizar las conversiones
+        km = distanciaMetros / 1000;
+        cm = distanciaMetros * 100;
+        mm = distanciaMetros * 1000;
+
+        // Mostrar el resultado
+        Console.WriteLine("Distancia: " + km + " km, " + cm + " cm, " + mm + " mm");
+    }
+}
