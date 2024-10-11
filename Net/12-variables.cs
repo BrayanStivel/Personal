cff83bd7bb4c2ca76f006aa09e61d9752bb6ba84
BrayanStivel <167356321+BrayanStivel@users.noreply.github.com>
2024-10-11 14:06:30 -0500

    Update 12-variables.cs

--- Net/12-variables.cs
+++ Net/12-variables.cs
@@ -1,7 +1,24 @@
-double cateto1, cateto2, hipotenusa;
-Console.WriteLine("Ingrese el primer cateto:");
-cateto1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Ingrese el segundo cateto:");
-cateto2 = double.Parse(Console.ReadLine());
-hipotenusa = Math.Sqrt(cateto1 * cateto1 + cateto2 * cateto2);
-Console.WriteLine("La hipotenusa es: " + hipotenusa);
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declaracion de variables
+        double cateto1, cateto2, hipotenusa;
+
+        // Solicitar al usuario que ingrese el primer cateto
+        Console.WriteLine("Ingrese el primer cateto:");
+        cateto1 = double.Parse(Console.ReadLine());
+
+        // Solicitar al usuario que ingrese el segundo cateto
+        Console.WriteLine("Ingrese el segundo cateto:");
+        cateto2 = double.Parse(Console.ReadLine());
+
+        // Calcular la hipotenusa
+        hipotenusa = Math.Sqrt(cateto1 * cateto1 + cateto2 * cateto2);
+
+        // Mostrar el resultado
+        Console.WriteLine("La hipotenusa es: " + hipotenusa);
+    }
+}
