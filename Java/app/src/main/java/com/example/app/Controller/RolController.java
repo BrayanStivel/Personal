9830a348e34a1054bafca91a48cf8b185eb19b82
BrayanStivel <167356321+BrayanStivel@users.noreply.github.com>
2024-10-18 17:08:49 -0500

    Add files via upload

---
+++ Java/app/src/main/java/com/example/app/Controller/RolController.java
@@ -0,0 +1,59 @@
+package com.example.app.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.app.Entity.Rol;
+import com.example.app.Services.RolServices;
+
+@Controller
+@RequestMapping("/roles")
+public class RolController {
+
+    @Autowired
+    private RolServices rolServices;
+
+    @GetMapping
+    public String listarRoles(Model model) {
+        List<Rol> roles = rolServices.obtenerTodos();
+        model.addAttribute("roles", roles);
+        return "roles/roles"; 
+    }
+
+    @GetMapping("/{id}")
+    public String detalleRol(@PathVariable Long id, Model model) {
+        Optional<Rol> rol = rolServices.obtenerPorId(id);
+        model.addAttribute("rol", rol.orElse(null));
+        return "roles/detalleRol"; 
+    }
+
+    @GetMapping("/nuevo")
+    public String crearNuevoRol(Model model) {
+        model.addAttribute("rol", new Rol());
+        return "roles/nuevoRol"; 
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(@ModelAttribute Rol rol) {
+        rolServices.guardar(rol);
+        return "redirect:/roles"; 
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable Long id) {
+        rolServices.eliminar(id);
+        return "redirect:/roles"; 
+    }
+    
+
+    
+}
---
+++ Java/app/src/main/java/com/example/app/Services/EmpleadoServices.java
@@ -0,0 +1,33 @@
+package com.example.app.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.app.Entity.Empleado; // Asegúrate de importar esta clase
+import com.example.app.Repository.EmpleadoRepository;
+
+@Service
+public class EmpleadoServices {
+
+    @Autowired // Añade esta anotación para la inyección de dependencias
+    private EmpleadoRepository empleadoRepository;
+
+    public List<Empleado> obtenerTodos() {
+        return empleadoRepository.findAll();
+    }
+
+    public Optional<Empleado> obtenerPorId(Long id) {
+        return empleadoRepository.findById(id);
+    }
+
+    public Empleado guardar(Empleado empleado) {
+        return empleadoRepository.save(empleado);
+    }
+
+    public void eliminar(Long id) {
+        empleadoRepository.deleteById(id);
+    }
+}
